278eafadb3d2bd5fdeb673a41dfe80bafab47b1f
kherin <kherin.j.s.bundhoo@gmail.com>
2019-10-30 16:51:30 +0400

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World !');
+    res.send('Hello I"m kherin and i sit next to a racist !');
 });
 
 app.listen(PORT, () => {});
