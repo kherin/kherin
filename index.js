c75da039d6b80e970706eef2722f3fd5b694c459
kherin <kherin.j.s.bundhoo@gmail.com>
2019-12-14 05:53:17 +0400

    ein

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'src/public')));
 
 
 app.get('/', (req, res) => {
-    res.status(200).send('index');
+    res.sendFile(path.join(__dirname + 'src/views/index.html'));
 });
 
 
