1531ec9a7a06b79890d02c20b7d1506329991cc1
kherin <kherin.j.s.bundhoo@gmail.com>
2019-12-14 05:35:36 +0400

    ein

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ var app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+
 app.get('/', (req, res) => {
-    res.send('ein!');
+    res.status.send('index');
 });
+
+
 
 app.listen(PORT, () => {});
